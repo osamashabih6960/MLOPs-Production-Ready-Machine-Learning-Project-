32c6df523c991ba16f13285460c44e124bab9c39
shabihosama@gmail.com <shab>
2026-01-26 19:12:45 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.0",
+    author="Osama",
+    author_email="osamashabih.jamia@gmail.com",
+    package=find_packages()
+    
+)
+
