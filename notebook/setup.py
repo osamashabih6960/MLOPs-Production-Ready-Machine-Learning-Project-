fdfd755979df9d3a4d749f7e5684385840b79532
shabihosama@gmail.com <shab>
2026-01-29 13:16:31 +0530

    Merge remote and local project

---
+++ notebook/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.0",
+    author="Osama",
+    description="End-to-End Production Ready MLOps Project",
+    author_email="osamashabih.jamia@gmail.com",
+    package=find_packages(),
+    python_requires=">=3.9",
+
+    
+)
+
